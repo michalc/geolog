3ea9c3f2bdefe32cc3ae20f612e90f305b211fd2
Michal Charemza <michalcharemza@gmail.com>
2016-10-12 07:31:51 +0100

    Tidy require

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const childProcess = require('child_process');
+const concurrent = require('concurrent-transform');
 const gulp = require('gulp');
+const awspublish = require('gulp-awspublish');
+const stream = require('stream');
+const zip = require('gulp-zip');
 
 AWS.config.region = 'eu-west-1';
 AWS.config.credentials = new AWS.SharedIniFileCredentials({profile: 's3-geolog'});
 const apigateway = new AWS.APIGateway();
 const lambda = new AWS.Lambda();
 
+const exec = childProcess.exec;
+
+const LAMBDA_NAME = 'geolog'
+const LAMBDA_ALIAS = 'prod'
 const BUILD_DIR = 'build';
 const API_GATEWAY_ID = '1jxogzz6a3';
+
+function updateFunctionCodeAndAlias(zippedCode, functionName, functionAlias) {
+  return lambda.updateFunctionCode({
+    Publish: true,
+    FunctionName: 'geolog',
+    ZipFile: zippedCode
+  }).promise().then(function(resource) {
+    return lambda.updateAlias({
+      FunctionName: functionName,
+      FunctionVersion: resource.Version,
+      Name: functionAlias
+    }).promise();
+  });
+}
 
 // One-time task
 gulp.task('permit-lambda', function() {
@@ -23,30 +46,13 @@ gulp.task('permit-lambda', function() {
 });
 
 gulp.task('deploy-lambda', function(cb) {
-  const stream = require('stream');
-  const zip = require('gulp-zip');
-
-  function putFunction(contents) {
-    return lambda.updateFunctionCode({
-      Publish: true,
-      FunctionName: 'geolog',
-      ZipFile: contents
-    }).promise().then(function(resource) {
-      return lambda.updateAlias({
-        FunctionName: 'geolog',
-        FunctionVersion: resource.Version,
-        Name: 'prod'
-      }).promise();
-    });
-  }
-
   const files = gulp.src(['src/back/endpoint.js']);
   return files
     .pipe(zip('endpoint.zip'))
     .pipe(stream.Transform({
       objectMode: true,
       transform: function(file, enc, cb) {
-        putFunction(file.contents).then(function() {
+        updateFunctionCodeAndAlias(file.contents, LAMBDA_NAME, LAMBDA_ALIAS).then(function() {
           cb();
         }, function(err) {
           cb(err);
@@ -56,15 +62,12 @@ gulp.task('deploy-lambda', function(cb) {
 });
 
 gulp.task('validate-api', function(cb) {
-  const exec = require('child_process').exec;
   exec('./node_modules/swagger-tools/bin/swagger-tools validate ./src/api/schema.yaml', function (err, stdout, stderr) {
     cb(err);
   });
 });
 
 gulp.task('deploy-api', function (cb) {
-  const stream = require('stream');
-
   function putApi(yaml) {
     return apigateway.putRestApi({
       body: yaml,
@@ -99,9 +102,6 @@ gulp.task('build-front', function() {
 });
 
 gulp.task('deploy-front', function() {
-  const concurrent = require('concurrent-transform');
-  const awspublish = require('gulp-awspublish');
-
   const publisher = awspublish.create({
     region: 'eu-west-1',
     params: {
