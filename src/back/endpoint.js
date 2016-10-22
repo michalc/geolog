628750efc36ef104f26211891b9629754c6ff00e
Michal Charemza <michalcharemza@gmail.com>
2016-10-22 22:04:30 +0100

    Annoyingly can't transform requests for proxied requests

--- src/back/endpoint.js
+++ src/back/endpoint.js
@@ -7,21 +7,25 @@ exports.handler = (event, context, callback) => {
     body: err ? err.message : JSON.stringify(res),
     headers: {
       'Content-Type': 'application/json',
-    },
+      // Proxy integrations can't transform the response
+      'Access-Control-Allow-Headers' : "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+      'Access-Control-Allow-Methods' : "*",
+      'Access-Control-Allow-Origin' : "*"
+    }
   });
 
   switch (event.httpMethod) {
     case 'DELETE':
-      done(null, 'Received DELETE');
+      done(null, {'message': 'Received DELETE'});
       break;  
     case 'GET':
-      done(null, 'Received GET');
+      done(null, {'message': 'Received GET'});
       break;
     case 'POST':
-      done(null, 'Received POST');
+      done(null, {'message': 'Received POST'});
       break;
     case 'PUT':
-      done(null, 'Received PUT');
+      done(null, {'message': 'Received PUT'});
       break;
     default:
       done(new Error(`Unsupported method "${event.httpMethod}"`));
