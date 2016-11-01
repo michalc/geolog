d7118b38985cc86b1eec3df56de09b972257a7a2
Michal Charemza <michalcharemza@gmail.com>
2016-11-01 13:42:26 +0000

    Test E2E from base

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -1,6 +1,6 @@
 describe('geolog', () => {
   it('has the title geolog', () => {
-    browser.url('http://localhost:8080/');
+    browser.url('/');
     const title = browser.getTitle();
     title.should.be.equal('GeoLog');
   });
