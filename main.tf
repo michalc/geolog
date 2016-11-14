beccd3767866015a0bb1cf29fbfc9f61d7e35b43
Michal Charemza <michalcharemza@gmail.com>
2016-11-14 22:10:14 +0000

    Need to output in main module

--- main.tf
+++ main.tf
@@ -5,3 +5,7 @@ provider "aws" {
 module "infrastructure" {
   source = "./src/infrastructure"
 }
+
+output "aws_api_gateway_rest_api.geolog.id" {
+  value = "${module.infrastructure.aws_api_gateway_rest_api.geolog.id}"
+}
