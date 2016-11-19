514941e3ea2e61020895b421fc4c0fdf4277d688
Michal Charemza <michalcharemza@gmail.com>
2016-11-19 01:03:12 +0000

    Access to buckets is on gateway. No need for bucket policy

--- src/infrastructure/aws_s3_bucket.green_geolog.co.tf
+++ src/infrastructure/aws_s3_bucket.green_geolog.co.tf
@@ -1,30 +1,7 @@
 resource "aws_s3_bucket" "green_geolog_co" {
   bucket = "green.geolog.co"
-  acl = "public-read"
   website {
     index_document = "index.html"
-  }
-}
-
-resource "aws_s3_bucket_policy" "green_geolog_co" {
-  bucket = "${aws_s3_bucket.green_geolog_co.bucket}"
-  policy = "${data.aws_iam_policy_document.green_geolog_co.json}"
-}
-
-data "aws_iam_policy_document" "green_geolog_co" {
-  statement {
-    sid = "AddPerm"
-    effect = "Allow"
-    principals = [{
-      type = "AWS"
-      identifiers = ["*"]
-    }]
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.green_geolog_co.bucket}/*"
-    ]
   }
 }
 
