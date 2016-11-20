72c28718f3a224412b6d7a9d550ab43f9d812bde
Michal Charemza <michalcharemza@gmail.com>
2016-11-20 19:47:02 +0000

    Start of a bit of react file choosing

--- src/front/assets/app.jsx
+++ src/front/assets/app.jsx
@@ -31,9 +31,66 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 class GeoLog extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleFileSelect = this.handleFileSelect.bind(this);
+    this.state = {
+      files: []
+    }
+  }
+
+  handleFileSelect(e) {
+    this.setState(prevState => {
+      return {
+        files: [e]
+      }
+    });
+  }
+
   render() {
     return (
-      <p>GeoLog</p>
+      <div>
+        <h1>GeoLog</h1>
+        <FileButton onFileSelect={this.handleFileSelect} />
+        <FileList files={this.state.files} />
+      </div>
+    );
+  }
+}
+
+class FileButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }   
+
+  handleChange(e) {
+    this.props.onFileSelect(this.input.files[0]);
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.handleChange}
+          type="file"
+          ref={(input) => { this.input = input; }}
+        />
+      </div>
+    );
+  }
+}
+
+class FileList extends React.Component {
+  render() {
+    const listItems = this.props.files.map((file, i) =>
+      <li key={i}>{ file.name }</li>
+    );
+    return (
+      <ul>
+        { listItems }
+      </ul>
     );
   }
 }
