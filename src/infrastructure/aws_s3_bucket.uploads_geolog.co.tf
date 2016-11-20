deaf5f3f0afa3b75977b1e83f0faaf6d71295723
Michal Charemza <michalcharemza@gmail.com>
2016-11-20 09:23:36 +0000

    s3 bucket

---
+++ src/infrastructure/aws_s3_bucket.uploads_geolog.co.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "uploads_geolog_co" {
+  bucket = "uploads.geolog.co"
+
+  cors_rule {
+    allowed_headers = [
+      "x-amz-*"
+    ]
+    allowed_methods = [
+      "PUT",
+    ]
+    allowed_origins = [
+      "localhost:8080",
+      "certification.geolog.co",
+      "geolog.co",
+    ]
+    # Options requests needed every time 
+    max_age_seconds = 0
+  }
+}
+
+data "aws_iam_policy_document" "geolog_cognito_upload" {
+  statement {
+    sid = ""
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${aws_s3_bucket.uploads_geolog_co.arn}/$${cognito-identity.amazonaws.com:sub}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "geolog_cognito_upload" {
+  name = "geolog_cognito_upload"
+  policy = "${data.aws_iam_policy_document.geolog_cognito_upload.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "geolog_cognito_upload" {
+  role = "${aws_iam_role.geolog_cognito_unauthenticated.name}"
+  policy_arn = "${aws_iam_policy.geolog_cognito_upload.arn}"
+}
