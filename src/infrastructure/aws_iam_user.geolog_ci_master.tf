bd4dfe869812562443ab535987d6c0a5e2ff379d
Michal Charemza <michalcharemza@gmail.com>
2016-11-29 21:43:31 +0000

    Clear that the user is for the master build

--- src/infrastructure/aws_iam_user.geolog_ci_master.tf
+++ src/infrastructure/aws_iam_user.geolog_ci_master.tf
@@ -1,18 +1,27 @@
-resource "aws_iam_user" "geolog_ci" {
-  name = "geolog-ci"
+# The user that builds and deploys the system.
+#
+# The credentials for this user should *not* be made available to
+# PRs, since that would result in unreviewed code running on the
+# production system
+# 
+# Since PRs have read access to the state file, the credentials
+# for this user must be created manually
+
+resource "aws_iam_user" "geolog_ci_master" {
+  name = "geolog_ci_master"
 }
 
-resource "aws_iam_policy" "geolog_ci" {
-  name = "geolog_ci"
-  policy = "${data.aws_iam_policy_document.geolog_ci.json}"
+resource "aws_iam_policy" "geolog_ci_master" {
+  name = "geolog_ci_master"
+  policy = "${data.aws_iam_policy_document.geolog_ci_master.json}"
 }
 
-resource "aws_iam_user_policy_attachment" "geolog_ci" {
-  user = "${aws_iam_user.geolog_ci.name}"
-  policy_arn = "${aws_iam_policy.geolog_ci.arn}"
+resource "aws_iam_user_policy_attachment" "geolog_ci_master" {
+  user = "${aws_iam_user.geolog_ci_master.name}"
+  policy_arn = "${aws_iam_policy.geolog_ci_master.arn}"
 }
 
-data "aws_iam_policy_document" "geolog_ci" {
+data "aws_iam_policy_document" "geolog_ci_master" {
   statement {
     effect = "Allow"
     actions = [
