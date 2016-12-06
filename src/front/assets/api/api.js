35022735d7a14e618d4edb98b5d477ee449be275
Michal Charemza <michalcharemza@gmail.com>
2016-12-06 08:38:21 +0000

    Upload file to unique key

--- src/front/assets/api/api.js
+++ src/front/assets/api/api.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const generateProbablyUniqueGuid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
 class Api {
   constructor(
     AWS,
@@ -40,12 +47,18 @@ class Api {
 
   upload(file) {
     this.creds.then((credentials) => {
+      // We are only allowed to overwrite keys
+      // with the current user's id as a prefix. At
+      // worst, a user can mess-up their own previous
+      // upload, so a probably unique ID is enough
+      const keySuffix = generateProbablyUniqueGuid();
+
       const s3 = new this.AWS.S3({
         credentials: credentials
       });
       s3.putObject({
         Bucket: this.uploadBucket,
-        Key: credentials.identityId + '/some-key',
+        Key: credentials.identityId + '/' + keySuffix,
         Body: file
       }).promise().then(() => {
       }, () => {
