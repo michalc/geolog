3edf5430a6c4d302eab1a9cbb7bf3e9634f064db
Michal Charemza <michal@charemza.name>
2017-09-26 14:05:28 +0100

    Stack of back-end as haskell

---
+++ src/back/app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Lib (runApp)
+
+main :: IO ()
+main = runApp
---
+++ src/back/src/Lib.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+module Lib
+    ( runApp
+    ) where
+
+import           Data.Text (Text)
+import           Yesod
+
+data Person = Person
+  { name :: Text 
+  , age  :: Int
+  }
+
+instance ToJSON Person where
+  toJSON Person {..} = object
+    [ "name" .= name
+    , "age"  .= age
+    ]
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/      HomeR  GET
+/other OtherR GET
+|]
+
+instance Yesod App
+
+getHomeR :: Handler Value
+getHomeR = returnJson $ Person "Michal" 34
+
+getOtherR :: Handler Value
+getOtherR = returnJson $ Person "Matt" 25
+
+runApp :: IO ()
+runApp = warp 3000 App
