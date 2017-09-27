df572d1c2565fadb04832064c81fa859e9e374de
Michal Charemza <michal@charemza.name>
2017-09-27 09:39:00 +0100

    First attempt at circle to push to repo and deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+SHA1=$1
+
+docker build --rm=false -t 772663561820.dkr.ecr.eu-west-1.amazonaws.com/geolog:$SHA1 src/back
+
+# Push image to ECR
+$(aws ecr get-login --region <region>)
+docker push 772663561820.dkr.ecr.eu-west-1.amazonaws.com/geolog:$SHA1
+
+# Create new Elastic Beanstalk version
+EB_BUCKET=deploy.geolog.co
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE --region eu-west-1
+aws elasticbeanstalk create-application-version --application-name geolog \
+    --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE \
+    --region eu-west-1
+
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name geolog-blue \
+    --version-label $SHA1 \
+    --region eu-west-1
